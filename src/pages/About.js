3004cae6c551dba528fa4e71fc307fdc90ac9a88
shubhambartwal <shubhambartwal58@gmail.com>
2024-09-02 11:21:34 +0530

    basics of routing

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>Voluptate nostrud id consequat ut irure fugiat laborum nisi nulla. Nostrud eiusmod proident adipisicing consequat labore. Dolor ex magna eu irure aute sunt.
+
+Eiusmod dolor exercitation elit ex sit amet Lorem aliquip commodo officia. Laborum nisi voluptate esse amet officia minim occaecat enim. Mollit cupidatat id et consectetur veniam ad fugiat mollit excepteur nostrud officia Lorem. Dolor ullamco exercitation dolore culpa esse ad sit ullamco labore commodo mollit qui reprehenderit. Exercitation nostrud tempor culpa reprehenderit sunt ea ut ea consectetur deserunt commodo nostrud. Occaecat tempor consectetur veniam eu fugiat excepteur incididunt sit. Deserunt exercitation ad est nulla eiusmod enim aliquip fugiat velit.
+
+Sit minim cillum consectetur dolore adipisicing commodo qui cillum irure officia. Commodo nisi pariatur pariatur aliquip adipisicing. Ad proident incididunt id officia fugiat id. Dolore ipsum sint occaecat amet velit eiusmod quis excepteur enim enim ut eu sint sint. Proident laboris proident amet nulla velit nisi ad sint laborum laboris exercitation. Laborum elit incididunt Lorem magna ullamco enim ex nostrud mollit quis labore dolore incididunt. Proident deserunt ipsum veniam ex voluptate id.
+
+Cillum magna velit et aute. Sint magna duis ut et reprehenderit aute Lorem elit dolore. Labore et aliqua fugiat cillum nostrud nisi. Lorem sint duis non aliquip laboris veniam consectetur.
+
+Nisi nisi exercitation velit veniam labore nostrud ipsum quis sint culpa tempor do cillum. Tempor fugiat et consequat in reprehenderit id est id exercitation consectetur. Id commodo laboris culpa amet sint tempor. Ea sint cillum dolor pariatur commodo reprehenderit aliqua. Commodo excepteur eiusmod nisi aute fugiat deserunt.</p></div>
+  )
+}
+
+export default About
---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>HOME</h1>
+        <p>Consequat anim proident in tempor sint et dolore veniam. Dolor do culpa amet aliquip exercitation Lorem aliqua cillum officia. Laborum elit exercitation excepteur incididunt dolore minim. Labore eiusmod do excepteur Lorem voluptate eu incididunt occaecat Lorem dolor labore nulla do. Laborum aliquip velit enim ad aute ea incididunt minim duis laborum ad commodo aute occaecat.
+
+Sunt fugiat aliqua quis pariatur eiusmod est amet dolore est et duis. Pariatur aute excepteur culpa ea in. Velit adipisicing ex consequat deserunt tempor commodo.
+
+Esse non exercitation aute sit Lorem et. Dolore velit cillum fugiat ipsum in culpa. Non duis consectetur cillum dolor deserunt nostrud ex est duis amet culpa velit consequat. Ex magna proident reprehenderit cupidatat et cupidatat ad laboris consectetur eiusmod consectetur enim nulla. Fugiat duis magna esse sit sit esse ad minim do nostrud est. Ea in magna ipsum sit commodo elit adipisicing ea.
+
+Do excepteur voluptate eiusmod irure sint veniam reprehenderit qui nisi. Id elit velit adipisicing laborum nulla labore nisi sit cillum nulla enim. Dolor culpa aute labore exercitation deserunt duis mollit adipisicing in ipsum ut.
+
+Excepteur elit reprehenderit enim pariatur aliquip. Culpa tempor adipisicing cillum laboris labore exercitation deserunt esse elit occaecat irure fugiat magna. Culpa aliquip commodo mollit officia elit duis excepteur do.
+
+Aute et minim dolore dolor enim quis. Proident cupidatat id nulla velit ea qui nostrud dolore Lorem dolore aute. Eiusmod incididunt dolor proident cillum esse velit aliquip duis esse aliquip in reprehenderit adipisicing. Ea eiusmod ea esse amet enim velit mollit esse eiusmod est Lorem consequat commodo. Incididunt esse adipisicing id laboris velit minim officia ullamco magna. Ipsum commodo sint quis cupidatat sint voluptate labore nulla fugiat nisi id.
+
+Eu ad in enim tempor officia minim esse qui nisi incididunt proident anim velit. Irure est commodo ea eu est. Ea mollit incididunt quis incididunt do reprehenderit ut duis. Nisi dolore dolor pariatur laboris minim cillum ad. Sit labore officia cillum duis in ullamco minim quis eiusmod. Incididunt reprehenderit quis ut id id laboris adipisicing quis est est. Ex deserunt ullamco do non elit est labore elit eu.
+
+Consectetur excepteur nostrud fugiat sint eiusmod duis non occaecat et culpa elit ullamco. Ullamco dolore quis sunt dolore. Et aute adipisicing nostrud qui commodo ea eu eiusmod et sunt pariatur cillum. Labore sunt in labore qui irure magna. Dolor ipsum occaecat quis sint cupidatat laborum et.</p></div>
+  )
+}
+
+export default Home
